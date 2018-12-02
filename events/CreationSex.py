acc8e58df61bd2eab0d2e6e543f3e3278fac0cb5
Sebastian Muenzner <munzner@kth.se>
2018-12-02 13:15:59 +0100

    * decisions are saved

--- events/CreationSex.py
+++ events/CreationSex.py
@@ -2,11 +2,19 @@ from event_base.event import EventBase
 
 
 class CreationSex(EventBase):
+    buttons = [{'text': 'test_1', 'next_event_id': 'creation_age', 'decision_id': 1},
+               {'text': 'test_2', 'next_event_id': 'creation_age', 'decision_id': 2},
+               {'text': 'test_3', 'next_event_id': 'creation_age', 'decision_id': 3}]
+
     def __init__(self, chat_id: int):
         super().__init__(chat_id=chat_id, prev_event_ids=['hello', 'hello'], event_id="creation_sex",
-                         message_text="hello", buttons=[{'text': 'test_1', 'next_event_id': 'creation_age'},
-                                                        {'text': 'test_2', 'next_event_id': 'creation_age'},
-                                                        {'text': 'test_3', 'next_event_id': 'creation_age'}])
+                         message_text="hello", buttons=self.buttons)
 
+    @staticmethod
     def is_available(profile):
         return True
+
+    @staticmethod
+    def react(profile, decision_id):
+        profile['basic']['sex'] = int(decision_id)
+        return profile
--- events/CreationUserName.py
+++ events/CreationUserName.py
@@ -2,12 +2,19 @@ from event_base.event import EventBase
 
 
 class CreationUserName(EventBase):
+    buttons = [{'text': 'Kai', 'next_event_id': 'creation_age', 'decision_id': 'Kai'},
+               {'text': 'Martin', 'next_event_id': 'creation_age', 'decision_id': 'Martin'},
+               {'text': 'Joni', 'next_event_id': 'creation_age', 'decision_id': 'Joni'}]
+
     def __init__(self, chat_id: int):
         super().__init__(chat_id=chat_id, prev_event_ids=['start'], event_id="creation_user_name",
-                         message_text="Choose a name", buttons=[{'text': 'Kai', 'next_event_id': 'creation_age',
-                                                                 'callback_data': '/next'},
-                                                                {'text': 'Martin', 'next_event_id': 'creation_age'},
-                                                                {'text': 'Joni', 'next_event_id': 'creation_age'}])
+                         message_text="Choose a name", buttons=self.buttons)
 
+    @staticmethod
     def is_available(profile):
         return True
+
+    @staticmethod
+    def react(profile, decision_id):
+        profile['basic']['name'] = decision_id
+        return profile
